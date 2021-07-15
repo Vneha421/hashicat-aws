41bfc11ab803c4f673204e0b8dfb563ffad7cb55
Neha Verma <vneha.421@gmail.com>
2021-07-15 22:11:36 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,7 @@
 module "s3-bucket" {
   source  = "app.terraform.io/NEHA-CHIP/s3-bucket/aws"
   version = "2.6.0"
-  # insert required variables here
   bucket_prefix = var.prefix
+  # insert required variables here
+  
 }
